ac7c23a402fe357735f42eada1c4da5b07bf4f46
ngoctu280801 <63794942+ngoctu280801@users.noreply.github.com>
2021-12-17 10:39:24 +0700

    Lấy dữ liệu trang thanh toán + điều kiện thanh toán

--- CovidManagementSystem/src/view/Payment.java
+++ CovidManagementSystem/src/view/Payment.java
@@ -2,6 +2,9 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
@@ -18,13 +21,31 @@ import java.awt.Font;
 import javax.swing.BoxLayout;
 import javax.swing.SwingConstants;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.UnknownHostException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
 
 import javax.swing.JTextField;
 
 import model.DbInteraction;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class Payment extends JDialog {
+	private DbInteraction dbi;
 	private String usrName;
 	private final JPanel pnTransHis = new JPanel();
 	private DefaultTableModel dtm;
@@ -33,6 +54,9 @@ public class Payment extends JDialog {
 	private JButton btnPay;
 	private static JLabel lblBalance, lblDebt;
 	private final String pathToTrustStore;
+	private final String pwd;
+	private final String serverName;
+	private final int port;
 
 	public Payment(DbInteraction dbi, String usrName, String path, String pwd, String svName, int port) {
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -41,12 +65,18 @@ public class Payment extends JDialog {
 			//JOptionPane.showMessageDialog(null, "Không tìm thấy " + path);
 			dispose();
 		}
+		this.dbi = dbi;
 		this.usrName = usrName;
 		pathToTrustStore = path;
+		this.pwd = pwd;
+		this.serverName = svName;
+		this.port = port;
 		System.setProperty("javax.net.ssl.trustStore", pathToTrustStore);
 		System.setProperty("javax.net.ssl.trustStorePassword", pwd);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		addControls();
+		addEvents();
+		reloadData();
 		setLocationRelativeTo(null);
 	}
 	private void addControls(){
@@ -141,8 +171,218 @@ public class Payment extends JDialog {
 		pnDebt.add(lblDebt);
 		
 	}
-	
+	private void addEvents(){
 		
-	
-	
+//		btnPay.addMouseListener(new MouseAdapter() {
+//			@Override
+//			
+//			public void mouseClicked(MouseEvent arg0) {
+//				if(btnPay.isEnabled()) { 
+//				Runnable connect = new Runnable(){
+//					public void run(){
+//						connectToServer();
+//					}
+//				};
+//				Thread t = new Thread(connect);
+//				t.start();
+//				reloadData();
+//				}
+//			}
+//		});
+		
+		
+		txtCredit.addKeyListener(new KeyAdapter() {
+			@Override
+			
+			public void keyReleased(KeyEvent arg0) {	
+				
+				txtCredit.setText(validateNum(new StringBuilder(txtCredit.getText())));
+				String credit = txtCredit.getText();
+				if(credit.equals("") || Integer.parseInt(credit.replace(",","")) < 20000 || 
+						 isDebtSmallerThanCreditMin()) {
+					btnPay.setEnabled(false);
+				}
+				else {btnPay.setEnabled(true);}
+			}
+			
+		});
+		
+	}
+	private void connectToServer(){
+		btnPay.setEnabled(false);
+		txtCredit.setEnabled(false);
+		SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+		SSLSocket sslSocket = null;
+		try {
+			sslSocket = (SSLSocket) ssf.createSocket(serverName, port);
+			OutputStreamWriter os = new OutputStreamWriter(sslSocket.getOutputStream());
+			PrintWriter out = new PrintWriter(os);
+			out.println(usrName + " " + txtCredit.getText().replace(",", ""));
+			os.flush();
+			
+			BufferedReader bfr = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+			String code = bfr.readLine();
+			sslSocket.close();
+			//codeToMessage(Integer.parseInt(code));
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Vui lòng xem lại địa chỉ server");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Server đang offline");
+		} 
+		
+		finally{
+			reloadData();
+		}
+	}
+//	private void codeToMessage(int code){
+//		if(code == 1){
+//			txtCredit.setText("");
+//			JOptionPane.showMessageDialog(null, "Thanh toán thành công");
+//		}
+//		else if(code == -1){
+//			JOptionPane.showMessageDialog(null, "Thanh toán thất bại");
+//		}
+//	}
+//	private void onOffPaymentBtn(){
+//		if(lblDebt.getText().equals("0 (VNĐ)")){
+//			txtCredit.setEnabled(false);
+//			btnPay.setEnabled(false);
+//		}
+//		else{
+//			txtCredit.setEnabled(true);
+//			btnPay.setEnabled(true);
+//		}
+//		if(txtCredit.getText().equals("")){
+//			btnPay.setEnabled(false);
+//		}
+//	}
+	private String validateNum(StringBuilder s){
+		for(int i = 0; i < s.length(); i++){
+			if(!Character.isDigit(s.charAt(i))){
+				s.deleteCharAt(i);
+				i--;
+			}
+		}
+		for(int i = s.length() - 3; i > 0; i-=3){
+			s.insert(i, ',');
+		}
+		return s.toString();
+	}
+	private void reloadData(){
+		Runnable getData = new Runnable(){
+			public void run(){
+				dtm.setRowCount(0);
+				getDataFromDb();
+			}
+		};
+		Thread t = new Thread(getData);
+		t.start();
+		
+		Runnable getDebtAndBalance = new Runnable(){
+			public void run(){
+				lblBalance.setText("");
+				lblDebt.setText("");
+				getDebtFromDb();
+				getBalanceFromDb();
+				lblDebt.setPreferredSize(lblBalance.getPreferredSize());
+			}
+		};
+		Thread t1 = new Thread(getDebtAndBalance);
+		t1.start();
+		
+		try {
+			t.join();
+			t1.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			//onOffPaymentBtn();
+		}
+	}
+	private void getDataFromDb(){
+		Statement[] stmt = new Statement[] {null};
+		ResultSet rs = dbi.query("select th.date_trans, th.credit, th.remaining_debt, th.remaining_balance "
+				+ "from transaction_history th join accounts a on a.id = th.from_id_acc "
+				+ "where a.usrname = '" + usrName + "'", stmt);
+		try {
+			while(rs.next()){
+				Vector<String> rowData = new Vector<String>();
+				rowData.add(rs.getString(1));
+				rowData.add(validateNum(new StringBuilder(rs.getString(2))));
+				rowData.add(validateNum(new StringBuilder(rs.getString(3))));
+				rowData.add(validateNum(new StringBuilder(rs.getString(4))));
+				dtm.addRow(rowData);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Không thể kết nối tới CSDL");
+		} finally {
+			try {
+				if(stmt[0] != null){
+					stmt[0].close();
+				}				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private void getDebtFromDb(){
+		Statement[] stmt = new Statement[] {null};
+		ResultSet rs = dbi.query("select d.debt from debt d "
+				+ "join accounts a on d.id_patient = a.id "
+				+ " where a.usrname = '" + usrName + "'", stmt);
+		try {
+			rs.next();
+			lblDebt.setText(validateNum(new StringBuilder(rs.getString(1))) + " (VNĐ)");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(stmt[0] != null){
+					stmt[0].close();
+				}				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private void getBalanceFromDb(){
+		Statement[] stmt = new Statement[] {null};
+		ResultSet rs = dbi.query("select pa.balance from payment_acc pa "
+				+ "join accounts a on pa.id_acc = a.id "
+				+ " where a.usrname = '" + usrName + "'", stmt);
+		try {
+			rs.next();
+			lblBalance.setText(validateNum(new StringBuilder(rs.getString(1))) + " (VNĐ)");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(stmt[0] != null){
+					stmt[0].close();
+				}				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private boolean isDebtSmallerThanCreditMin(){
+		if(20000 > lblToInt(lblDebt.getText())){
+			return true;
+		}
+		return false;
+	}
+	private int lblToInt(String s){
+		return Integer.parseInt(s.substring(0, s.length() - 6).replace(",", ""));
+	}
 }
