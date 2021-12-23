3cf071faeb1cb008ae6a1d231638627dd957b39a
vutuanhaigk123 <vutuanhaigk123@gmail.com>
2021-12-23 08:45:06 +0700

    log4J for server
    log4J for server

--- PaymentManagementSystem/src/model/Server.java
+++ PaymentManagementSystem/src/model/Server.java
@@ -1,34 +1,60 @@
 package model;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 
+import org.apache.log4j.Logger;
+
 public class Server {
 	public static boolean stop;
 	public static SSLServerSocket serverSocket;
-	public Server(){
+	private static Logger logger;
+	
+	
+	public Server(Logger logger){
+		
 		stop = false;
+		
+		this.logger = logger;
+		
 		serverSocket = null;
 	}
 	public void start(int port) throws IOException{
+		
+		// Khởi tạo SSL server socket factory
+		
+		
 		SSLServerSocketFactory ssf = 
-				(SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+							(SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+		
+		
+		// Tạo server socket SSL
 		serverSocket = (SSLServerSocket) ssf.createServerSocket(port);
+		
+		
 		System.out.println("Đã bật server");
+		
+		logger.debug("Server đang chạy");
+		
 		while(!stop){
+			// Luồng hiện tại accept kết nối
 			SSLSocket sslSocket = (SSLSocket) serverSocket.accept();
-			ClientConnection client = new ClientConnection(sslSocket, "localhost", "qlcovid", "root", "");
+			
+			// Các kết nối đã được accept đưa qua luồng khác để xử lí
+			ClientConnection client = new ClientConnection(sslSocket, 
+										"localhost", "qlcovid", "root", "", logger);
 			client.run();
 		}
+		
 		serverSocket.close();
+		
 		serverSocket = null;
+		
+		logger.debug("Đã tắt server");
+		
 		System.out.println("Đã tắt server");
 	}
 }
